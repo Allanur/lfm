af4577a9f6bb58ff81f971d0eaa2bc5c433baabd
Allanur <35625746+Allanur@users.noreply.github.com>
2022-03-01 11:53:32 +0500

    Update LfmStorageRepository.php

--- src/LfmStorageRepository.php
+++ src/LfmStorageRepository.php
@@ -26,13 +26,14 @@ class LfmStorageRepository
 
     public function rootPath()
     {
-        $adapter = $this->disk->getDriver()->getAdapter();
+//         $adapter = $this->disk->getDriver()->getAdapter();
 
-        if ($adapter instanceof CachedAdapter) {
-            $adapter = $adapter->getAdapter();
-        }
+//         if ($adapter instanceof CachedAdapter) {
+//             $adapter = $adapter->getAdapter();
+//         }
 
-        return $adapter->getPathPrefix();
+//         return $adapter->getPathPrefix();
+        return $this->path();
     }
 
     public function move($new_lfm_path)
